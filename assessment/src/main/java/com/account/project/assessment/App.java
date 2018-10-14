eb911a151ba4c32bb718721ddf0e4368e250e872
shashisrinivas <shashi.srinivas11@gmail.com>
2018-10-14 23:02:06 +0100

    First commit for the assessment project

---
+++ assessment/src/main/java/com/account/project/assessment/App.java
@@ -0,0 +1,13 @@
+package com.account.project.assessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
